f3c2b229860c8f8afc5160be931704299f676690
Santiago <santivelgasss@gmail.com>
2024-02-23 19:03:01 -0300

    Servicio de Actualizar

--- src/main/java/com/company/inventory/services/CategoryServicesImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServicesImpl.java
@@ -122,6 +122,7 @@ public class CategoryServicesImpl implements ICategoryService{
 				categorySearch.get().setDescription(category.getDescription());
 				
 				Category categoryToUpdate = categoryDao.save(categorySearch.get());
+				
 				if (categoryToUpdate != null) {
 					list.add(categoryToUpdate);
 					response.getCategoryResponse().setCategory(list);
@@ -134,7 +135,7 @@ public class CategoryServicesImpl implements ICategoryService{
 				
 			} else {
 				response.setMetadata("Respuesta negativa", "-1", "Categoria no actualizada");
-				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.BAD_REQUEST);
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
 			}	
 			
 		} catch (Exception e) {
